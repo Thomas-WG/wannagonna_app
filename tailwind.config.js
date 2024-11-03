1ee411b59501922b43c2f85c5780efbc9d4bca9b
Thomas-WG <thomas@wannagonna.org>
2024-11-04 00:02:46 +0900

    Installed Flowbite + sidebar creation + activity card

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+const flowbite = require("flowbite-react/tailwind");
 
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
     "./src/styles/**/*.css",
+    flowbite.content(),
   ],
   theme: {
     extend: {
@@ -12,7 +14,12 @@ module.exports = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        sans: ['Roboto', 'Arial', 'sans-serif'], // Adds Roboto as the main font
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    flowbite.plugin(),
+  ],
 };
