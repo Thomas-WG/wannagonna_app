fa32db2cda5f57faa492041af25159785b6d8e6f
Thomas-WG <thomas@wannagonna.org>
2024-11-04 10:59:15 +0900

    Added a expand/collapse function

---
+++ src/constant/config.js
@@ -0,0 +1,6 @@
+export const siteConfig = {
+  title: 'Wanna Gonna: Make a difference Today',
+  description:
+    'A free collaborative platform for volunteering, which brings together people skills and the needs of NGOs/NPOs, all around the world.',
+  url: 'https://wannagonna.org',
+};
