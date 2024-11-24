e224c5e1b565b4c3e27c21a22b06935a04293204
Thomas-WG <thomas@wannagonna.org>
2024-11-24 19:58:08 +0900

    Update locale.js
    adding the await on the cookies() as it’s async functions

--- src/utils/locale.js
+++ src/utils/locale.js
@@ -6,13 +6,13 @@ import {defaultLocale} from '@/i18n/config';
 const COOKIE_NAME = 'NEXT_LOCALE';
 
 export async function getUserLocale() {
-    const cookieStore = cookies();
+    const cookieStore = await cookies();
     const localeCookie = await cookieStore.get('NEXT_LOCALE');
   
   return localeCookie?.value || defaultLocale;
 }
 
 export async function setUserLocale(locale) {
-    const cookieStore = cookies();
+    const cookieStore = await cookies();
     await cookieStore.set(COOKIE_NAME, locale);
 }
